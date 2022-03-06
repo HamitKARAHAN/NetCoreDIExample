80a619138c0b901c74bc989047912fa062016c5a
Hamit KARAHAN <hamitkarahan10@gmail.com>
2022-03-06 13:11:43 +0300

    blank fixed

--- src/NetCore.DI.Services.Example/DateTimeService.cs
+++ src/NetCore.DI.Services.Example/DateTimeService.cs
@@ -10,6 +10,7 @@ namespace NetCore.DI.Services.Example
             _logger=logger;
             _logger.LogInformation("DateTime Service is running...");
         }
+
         public Task<TimeSpan> GetDateTime { get; init; } = Task.FromResult(DateTime.Now.TimeOfDay);
     }
 }
